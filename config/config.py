5c5c5b0ff03e0b7aabf4453d46f7d6d642e5770c
조재영 <jojaeyoung777@gmail.com>
2026-02-04 17:10:59 +0900

    Update config.py

--- config/config.py
+++ config/config.py
@@ -2,16 +2,16 @@
 
 # DMP 정보
 NDAP_INFO = {
-    "host": "10.10.10.225",
-    "port": 10000,
-    "username": "admin",
-    "password": "admin",
-    "auth": "LDAP",
+    "host": "00.00.00.000",
+    "port": 00000,
+    "username": "0",
+    "password": "0",
+    "auth": "0",
 }
 
 # slack 정보 (변경 필요)
-SLACK_TOKEN = "xoxb-2833641513623-9728915799682-73utTaRdDfZEd0gnMQV1fbts"
-SLACK_CHANNEL = "C09MCQ14DRU"
+SLACK_TOKEN = "0-0-0-0"
+SLACK_CHANNEL = "0"
 
 # test용 slack 정보 (변경 필요)
-SLACK_CHANNEL_TEST = "C09MX2L3MPZ"
+SLACK_CHANNEL_TEST = "0"
